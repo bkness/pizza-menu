1da8673fbb4a4042c8395bc3623fee1512ba0c1e
Brandon Kelly <kbrandon863@gmail.com>
2024-08-06 16:32:27 -0700

    Added props to Pizza component and applied styles across components

--- src/index.js
+++ src/index.js
@@ -73,11 +73,36 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spincai"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        prices={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+
+// Never nest your components but always declare them in the top level
+function Pizza(props) {
+  console.log(props);
+
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />{" "}
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 
@@ -98,17 +123,6 @@ function Footer() {
   );
 
   //   return React.createElement("footer", null, "We're currently open!");
-}
-
-// Never nest your components but always declare them in the top level
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
